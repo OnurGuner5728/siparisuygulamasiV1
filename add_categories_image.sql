20903686038fc4e2ef6cb420b5e25d374189a4f0
Onur Güner <onurguner.bjk@gmail.com>
2025-05-23 17:52:58 +0300

    Delete add_categories_image.sql

--- add_categories_image.sql
+++
@@ -1,8 +0,0 @@
--- Categories tablosuna image sütunu ekle
-ALTER TABLE categories 
-ADD COLUMN image TEXT;
-
--- Mevcut kategorilere varsayılan değerler ekle (isteğe bağlı)
-UPDATE categories 
-SET image = NULL 
-WHERE image IS NULL; 
